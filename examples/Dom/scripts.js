d5bad0106b62d8b70903b4851971a862aa5bc14d
Chris Watts <39687561+cj-watts@users.noreply.github.com>
2021-04-12 21:54:12 -0400

    Dom Exercise completed

--- examples/Dom/scripts.js
+++ examples/Dom/scripts.js
@@ -8,4 +8,14 @@ function main(){
     let link = document.querySelector('a');
     link.setAttribute('href',"https://www.example.com");
     link.innerText = "somewhere";
+    let hideMe = document.getElementById('hide-me');
+    hideMe.style.display = "none"
+    document.getElementById('show-me').style.display = "block"
+    let inputName = document.getElementById('name').value
+    let h1 = document.querySelector('h1');
+    h1.innerText = "Welcome " + inputName;
+    let radio = document.querySelector(
+        'input[name="button"]:checked'
+        );
+    console.log(radio);
 }
