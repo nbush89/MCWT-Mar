56c69c79d3039202d59a328b68e6043458844684
Chris Watts <39687561+cj-watts@users.noreply.github.com>
2021-06-17 19:03:38 -0400

    More of practice 5

--- Angular/assessment5Prac/src/app/practice1/practice1.component.ts
+++ Angular/assessment5Prac/src/app/practice1/practice1.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Name{
+  text: string;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-practice1',
   templateUrl: './practice1.component.html',
@@ -10,7 +15,16 @@ export class Practice1Component implements OnInit {
     "dark": false,
     "light": true
   }
+
   myString:string = "";
+  number1: string ='';
+  number2: string='';
+  sum:number=0;
+  names:Name[] =[
+    {text:"name1", checked:false},
+    {text:"name2", checked:false},
+    {text:"name3", checked:false}
+  ]
   constructor() { }
 
   ngOnInit(): void {
@@ -19,5 +33,11 @@ export class Practice1Component implements OnInit {
     this.buttonClass.dark = !this.buttonClass.dark
     this.buttonClass.light = !this.buttonClass.light
     this.myString += "CLICKED "
+  }
+  addNumbers(){
+    this.sum = parseFloat(this.number1) + parseFloat(this.number2);
+  }
+  checkName(index: number){
+    this.names[index].checked = !this.names[index].checked;
   }
 }
